97d39f82887a86561e7a10d50ac4b1f2e92e722e
Huy <touch@nguyenrk.com>
2021-12-11 17:03:36 +0700

    Add bio in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,22 @@ export default function HomePage(props: Props) {
 	return (
 		<Page>
 			<div className="max-w-2xl mx-auto space-y-4 pb-8">
-				{props.posts.map((post) => (
-					<PostCard key={post.slug} post={post} />
-				))}
+				<div className="prose prose-lg tight space-y-2">
+					<h1>Huy Nguyen</h1>
+					<h4>Senior Front-end Engineer</h4>
+					<p>
+						I am a minimalist, trying to do everything as simple as
+						possible. I do front-end for the most of the time,
+						besides that, I enjoy doing projects about blockchain,
+						cryptocurrency and NFT.
+					</p>
+				</div>
+				<div className="space-y-2">
+					<h1 className="font-bold text-3xl">Blogs</h1>
+					{props.posts.map((post) => (
+						<PostCard key={post.slug} post={post} />
+					))}
+				</div>
 			</div>
 		</Page>
 	);
